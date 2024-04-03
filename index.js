c573b54f2368c19dbc33bf5f11d45716be2ac1a8
ktrmzm <k.maezmac@gmail.com>
2024-04-03 19:13:43 +0900

    綺麗に

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const accessToken = process.env.TWITTER_ACCESS_TOKEN;
 const accessSecret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
 
 const client = new TwitterApi({
-  appKey,
-  appSecret,
-  accessToken,
-  accessSecret,
+    appKey,
+    appSecret,
+    accessToken,
+    accessSecret,
 });
 
 client.readWrite;
@@ -23,81 +23,66 @@ const app = express();
 
 const greet = async () => {
     const pool = new pg.Pool({
-        // Render.comのDBの接続情報に変える
         database: process.env.DATABASE,
         user: process.env.DATABASE_USER,
         password: process.env.DATABASE_PASSWORD,
         host: process.env.DATABASE_HOST,
-      
-        // Render.comのDBではSSLが求められる
         ssl: {
-          rejectUnauthorized: false, // 証明書の検証はいったん無しで
+            rejectUnauthorized: false,
         },
         max: 10,
-      });
+    });
 
     var random = 1000;
-      
-      pool.query(
+
+    pool.query(
         'SELECT * FROM public.newtable'
     ).then(result => {
-        // 結果データの表示
         if (result.rows) {
             var count = result.rows.length;
-            console.log("かうんと" + count);
-
             random = Math.floor(Math.random() * count);
-            console.log("らんだむ1" + random);
-            
         }
     })
-    .catch(err => {
-        console.log('err: ', err);
-    })
-    .then(() => {
-        console.log('お');
+        .catch(err => {
+            console.log('err: ', err);
+        })
+        .then(() => {
+            var sql = 'SELECT * FROM public.newtable WHERE number = ' + random;
+            pool.query(
+                sql
+            ).then(result => {
+                if (result.rows) {
+                    // result.rows.forEach((row) => {
+                    //     if(row.number)
+                    //     console.log(row);
+                    // });
+                    client.v2.tweet(result.rows[0].url + " #PR" + " #Amazon");
+                }
+            })
+                .catch(err => {
+                    console.log('err: ', err);
+                })
+                .then(() => {
+                    console.log('切断');
+                    pool.end();
+                });
+        });
 
-        var sql = 'SELECT * FROM public.newtable WHERE number = ' + random;
-    console.log("SQL " + sql);
-    pool.query(
-        sql
-    ).then(result => {
-        // 結果データの表示
-        console.log("う");
-        if (result.rows) {
-            // result.rows.forEach((row) => {
-            //     if(row.number)
-            //     console.log(row);
-            // });
-            console.log("あ");
-            client.v2.tweet(result.rows[0].url + " #PR" + " #Amazon");
-            console.log("い");
-        }
-    })
-    .catch(err => {
-        console.log('err: ', err);
-    })    
-    .then(() => {
-        console.log('切断2');
-        pool.end();
-    });    
-    });    
-    
 };
 
 app.get("/tweet", (req, res) => {
-    try{
+    try {
         greet();
-    } catch(err){
+    } catch (err) {
         console.log(err);
     }
     res.send('get');
 });
 
 app.get("/", (req, res) => {
-    try{
+    try {
         console.log("ログ定期実行")
-    } catch(err){
+    } catch (err) {
         console.log(err);
     }
     res.send('get');
