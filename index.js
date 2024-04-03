13004638df34f4c1b26eb38ec1b4badba0d644d3
ktrmzm <k.maezmac@gmail.com>
2024-04-03 16:32:35 +0900

    countに

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const greet = async () => {
         },
         max: 10,
       });
-      var count = await pool.query('SELECT * FROM public.newtable').rows.length;
+      var count = await pool.query('SELECT * FROM public.newtable').rows.count;
       console.log("count: " + count);
       if(count){
         var number = Math.floor(Math.random() * (count + 1));
