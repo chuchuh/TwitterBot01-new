3ffb30385bad767292685555d58e438106a84897
ktrmzm <k.maezmac@gmail.com>
2024-05-21 16:49:07 +0900

    楽天はあきらめ

--- index.js
+++ index.js
@@ -93,6 +93,40 @@ const execute = async (url) => {
 //     res.send('get');
 // });
 
+const rakuten = async () => {
+    var args = [
+        20,
+        30,
+        40
+    ]
+    var age = args[Math.floor(Math.random() * args.length)];
+    var random = Math.floor(Math.random() * 34) + 1;
+    var requestUrl = "https://app.rakuten.co.jp/services/api/IchibaItem/Ranking/20220601?applicationId=" + process.env.RAKUTEN_APP_ID
+        + "&age=" + age + "&sex=1&carrier=0&page=" + random + "&affiliateId=" + process.env.RAKUTEN_AFFILIATE_ID;
+    console.log(requestUrl);
+    await axios.get(requestUrl, {
+    }).then(async (response) => {
+        if (response.status !== 201) {
+            var randomNo = Math.floor(Math.random() * (response.data.Items.length));
+            var itemName = response.data.Items[randomNo].Item.itemName;
+            var catchcopy = response.data.Items[randomNo].Item.catchcopy;
+            var affiliateUrl = response.data.Items[randomNo].Item.affiliateUrl;
+            console.log(itemName);
+            console.log(catchcopy);
+            console.log(affiliateUrl);
+            var tweetText = itemName + catchcopy
+            client.v2.tweet(tweetText.substring(0, 90) + " " + affiliateUrl + " #楽天ROOM #楽天 #楽天市場 #ad #PR");
+            console.log("完了");
+
+        }
+    }).catch((error) => {
+        console.log(error);
+        return;
+    });
+
+
+};
+
 app.get("/tiktok4500", (req, res) => {
     try {
         execute(process.env.TIKTOK_4500_API_URL);
@@ -122,7 +156,7 @@ app.get("/amazon", (req, res) => {
 
 app.get("/rakuten", (req, res) => {
     try {
-        execute(process.env.RAKUTEN_API_URL);
+        rakuten();
     } catch (err) {
         console.log(err);
     }
